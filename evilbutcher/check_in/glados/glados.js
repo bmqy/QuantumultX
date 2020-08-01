a77acf70d946bd17327912e7f8af932e48ba9b1e
bmqy <qq88268459@126.com>
2020-08-01 11:53:47 +0800

    update

--- evilbutcher/check_in/glados/glados.js
+++ evilbutcher/check_in/glados/glados.js
@@ -1,13 +1,33 @@
 /*
 
 登陆链接：https://glados.rocks/，登陆即可获取Cookie。
+欢迎体验，邀请码：3JRG4-KSGZJ-8QPXF-8PPOO
 
+【Surge】
+-----------------
+[Script]
+GLaDOS签到 = type=cron,cronexp=5 0 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
+
+获取GLaDOS_Cookie = type=http-request, pattern=https:\/\/glados\.rocks\/api\/user\/status, script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
+
+【Loon】
+-----------------
+[Script]
+cron "5 0 * * *" tag=GLaDOS签到, script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
+
+http-request https:\/\/glados\.rocks\/api\/user\/status tag=获取GLaDOS_Cookie, script-path=https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
+
+
+【Quantumult X】
+-----------------
 [rewrite_local]
-https:\/\/glados\.rocks\/api\/user\/status url script-request-header https://github.com/evilbutcher/Quantumult_X/raw/master/check_in/glados/glados.js
+https:\/\/glados\.rocks\/api\/user\/status url script-request-header https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
 
 [task_local]
-1 0 * * * https://github.com/evilbutcher/Quantumult_X/raw/master/check_in/glados/glados.js
+1 0 * * * https://raw.githubusercontent.com/evilbutcher/Quantumult_X/master/check_in/glados/glados.js
 
+
+【All App MITM】
 hostname = glados.rocks
 */
 
@@ -30,7 +50,6 @@ var message = [];
   }
   await signin();
   await status();
-  $.done();
 })()
   .catch(e => {
     $.log("", `❌失败! 原因: ${e}!`, "");
